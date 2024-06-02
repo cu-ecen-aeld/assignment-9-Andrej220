48f329783e67fd672b2a1029e39a94ad84f3c75d
Andrey <andrey.zargarov@gmail.com>
2024-06-02 11:46:41 +0200

    assignment8, install dir change

--- base_external/package/aesd-assignments-socket/aesd-assignments-socket.mk
+++ base_external/package/aesd-assignments-socket/aesd-assignments-socket.mk
@@ -6,25 +6,25 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_SOCKET_VERSION = 0eedf304b6f2aaee29fb60a849fc2a370cfc9f9a
+AESD_ASSIGNMENTS_SOCKET_VERSION = 3fa291ee05731ac38af0d5520beee1f7ea7e1dd3
 #'#COMMIT VERSION NUMBER'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SOCKET_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Andrej220.git
-AESD_ASSIGNMENTS_SOCKET_OVERRIDE_SRCDIR = $(TOPDIR)/../server
+#AESD_ASSIGNMENTS_SOCKET_OVERRIDE_SRCDIR = $(TOPDIR)/../server
 #'#GITHUB REPOSITORY LINK'
 AESD_ASSIGNMENTS_SOCKET_SITE_METHOD = git
 AESD_ASSIGNMENTS_SOCKET_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_SOCKET_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_SOCKET_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -m 0755 $(@D)/aesdsocket $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 
 endef
 
